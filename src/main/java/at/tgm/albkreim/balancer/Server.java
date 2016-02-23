d15df788772053fc78e65f68dd07a5ab11bb126d
BuildTools <mreilaender@student.tgm.ac.at>
2016-02-23 16:53:46 +0100

    added author and date

--- src/main/java/at/tgm/albkreim/balancer/Server.java
+++ src/main/java/at/tgm/albkreim/balancer/Server.java
@@ -3,7 +3,8 @@ package at.tgm.albkreim.balancer;
 import at.tgm.albkreim.common.Connection;
 
 /**
- * mreilaender
+ * @author mreilaender
+ * @date: 23.02.2016
  */
 public abstract class Server {
     private Connection connection;
