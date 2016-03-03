c8a0e4c585591c57fcec565305f43093b336029f
BuildTools <mreilaender@student.tgm.ac.at>
2016-03-03 19:31:50 +0100

    implement compareTo

--- src/main/java/at/tgm/ablkreim/balancer/ServerLoad.java
+++ src/main/java/at/tgm/ablkreim/balancer/ServerLoad.java
@@ -6,14 +6,18 @@ import java.io.Serializable;
  * @author: mreilaender
  * @date: 23.02.2016
  */
-public class ServerLoad implements Serializable {
+public class ServerLoad implements Serializable,Comparable<ServerLoad> {
     public final double cpu, ram, drive, network;
 
     public ServerLoad(double cpu, double ram, double drive, double network) {
-
         this.cpu = cpu;
         this.ram = ram;
         this.drive = drive;
         this.network = network;
+    }
+
+    @Override
+    public int compareTo(ServerLoad other) {
+        return (int)((this.cpu + this.ram + this.drive + this.network) - (other.cpu + other.ram + other.drive + other.network));
     }
 }
