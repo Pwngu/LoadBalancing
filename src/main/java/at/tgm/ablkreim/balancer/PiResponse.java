9bd325aa6c41a6a318997d8f1cfd2305cef16423
Pwngu <klaus.ableitinger@gmail.com>
2016-03-03 18:49:52 +0100

    add PiRequest and Server

---
+++ src/main/java/at/tgm/ablkreim/balancer/PiResponse.java
@@ -0,0 +1,16 @@
+package at.tgm.ablkreim.balancer;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+public class PiResponse implements Serializable {
+
+    public final BigDecimal pi;
+    public final long id;
+
+    public PiResponse(BigDecimal pi, long id) {
+
+        this.pi = pi;
+        this.id = id;
+    }
+}
