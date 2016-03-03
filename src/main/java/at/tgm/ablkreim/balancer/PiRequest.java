e23882ceb59699ef8d503628e98b11cc070e652d
BuildTools <mreilaender@student.tgm.ac.at>
2016-03-03 17:12:55 +0100

    added constructor

--- src/main/java/at/tgm/ablkreim/balancer/PiRequest.java
+++ src/main/java/at/tgm/ablkreim/balancer/PiRequest.java
@@ -10,5 +10,12 @@ public class PiRequest implements Serializable {
     /* TODO Maybe rename to algorithm, then making an interface with constant variables describing the algorithm (e.g. Algorithms interface) */
     public static int LEIBNIZ = 1;
 
-    private int precision, begin, end, algorithmn;
+    public final int precision, begin, end, algorithmn;
+
+    public PiRequest(int precision, int begin, int end, int algorithmn) {
+        this.precision = precision;
+        this.begin = begin;
+        this.end = end;
+        this.algorithmn = algorithmn;
+    }
 }
