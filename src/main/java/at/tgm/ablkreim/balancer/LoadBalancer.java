03b0a07f54e047af62bdb9b34bb1af79565f1f9c
BuildTools <mreilaender@student.tgm.ac.at>
2016-03-03 20:19:23 +0100

    implement server list and algorithms decision

--- src/main/java/at/tgm/ablkreim/balancer/LoadBalancer.java
+++ src/main/java/at/tgm/ablkreim/balancer/LoadBalancer.java
@@ -10,8 +10,9 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.net.InetAddress;
 import java.net.ServerSocket;
-import java.net.Socket;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author: mreilaender
@@ -21,33 +22,49 @@ public class LoadBalancer {
     private static final Logger LOG = LogManager.getLogger(LoadBalancer.class);
 
     private LoadBalancerConfig loadBalancerConfig;
+    private LoadBalancingAlgorithm loadBalancingAlgorithm;
     private ServerSocket servers, clients;
+    private List<Server> connectedServers;
 
     public static void main(String[] args) {
         new LoadBalancer().start();
     }
 
     public LoadBalancer() {
+        this.connectedServers = new ArrayList<>();
         try {
-            URL url = getClass().getClassLoader().getResource("serv_config.json");
+            URL url = getClass().getClassLoader().getResource("server_config.json");
             if (url == null)
                 LOG.fatal("Cannot find config file");
             System.exit(1);
-
             loadBalancerConfig = new LoadBalancerConfig(new FileReader(url.getFile()));
         } catch (FileNotFoundException e) {
             LOG.fatal("Could not find file", e);
+        }
+        switch (loadBalancerConfig.getLoadBalancerAlgorithm()) {
+            case LoadBalancingAlgorithm.WEIGHTED_DISTRIBUTION:
+                this.loadBalancingAlgorithm = new WeightedDistribution();
+                break;
+            case LoadBalancingAlgorithm.LEAST_CONNECTION:
+                // TODO
+                break;
+            case LoadBalancingAlgorithm.RESPONSE_TIME:
+                // TODO
+                break;
+            case LoadBalancingAlgorithm.SERVER_LOAD:
+                // TODO
+                break;
         }
     }
 
     public void start() {
         try {
             LOG.info("Reading connfig");
-            InetAddress serverHost = InetAddress.getByName(loadBalancerConfig.getServIP());
+            InetAddress serverHost = InetAddress.getByName(loadBalancerConfig.getServerIP());
             InetAddress clientHost = InetAddress.getByName(loadBalancerConfig.getIP());
 
             LOG.info("Creating ServerSockets");
-            this.servers = new ServerSocket(loadBalancerConfig.getServPort(), 50, serverHost);
+            this.servers = new ServerSocket(loadBalancerConfig.getServerPort(), 50, serverHost);
             this.clients = new ServerSocket(loadBalancerConfig.getPort(), 50, clientHost);
 
             LOG.info("Waiting for new connections");
@@ -70,11 +87,7 @@ public class LoadBalancer {
         @Override
         public void run() {
             while (running) {
-                switch (loadBalancerConfig.getLbAlgorithm()) {
-                    case LoadBalancerAlogithms.LEAST_CONNECTION:
 
-
-                }
             }
         }
 
