2263279d2e55e46ed4fc0e5bacc381b1bceb448d
BuildTools <mreilaender@student.tgm.ac.at>
2016-03-03 20:23:27 +0100

    structure

---
+++ src/main/java/at/tgm/ablkreim/balancer/WeightedDistribution.java
@@ -0,0 +1,19 @@
+package at.tgm.ablkreim.balancer;
+
+import at.tgm.ablkreim.server.Server;
+
+/**
+ * @author: mreilaender
+ * @date: 03.03.2016
+ */
+public class WeightedDistribution implements LoadBalancingAlgorithm {
+    @Override
+    public void addServer(Server server) {
+
+    }
+
+    @Override
+    public void send(PiRequest piRequest) {
+
+    }
+}
