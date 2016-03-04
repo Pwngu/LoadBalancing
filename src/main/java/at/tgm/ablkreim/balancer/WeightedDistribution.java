6498d5e9384e21adbdf0e83d1a2a89f77011d0fe
BuildTools <mreilaender@student.tgm.ac.at>
2016-03-04 11:59:38 +0100

    add weighted distribution stub

--- src/main/java/at/tgm/ablkreim/balancer/WeightedDistribution.java
+++ src/main/java/at/tgm/ablkreim/balancer/WeightedDistribution.java
@@ -1,24 +1,33 @@
 package at.tgm.ablkreim.balancer;
 
-import at.tgm.ablkreim.server.Server;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+import java.util.Map;
+import java.util.TreeMap;
 
 /**
  * @author: mreilaender
  * @date: 03.03.2016
  */
 public class WeightedDistribution implements LoadBalancingAlgorithm {
+    public static final Logger LOGGER = LogManager.getLogger(WeightedDistribution.class);
+
+    public WeightedDistribution() { }
+
     @Override
     public void addServer(Server server) {
-
+        throw new NotImplementedException();
     }
 
     @Override
     public void removeServer(Server server) {
-
+        throw new NotImplementedException();
     }
 
     @Override
     public void send(PiRequest piRequest) {
-
+        throw new NotImplementedException();
     }
 }
